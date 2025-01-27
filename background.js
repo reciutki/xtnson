ba6e364176003a7df644c71f153b838048a681b1
patrykrosinski <patryk.rosinski@gforces.pl>
2025-01-27 16:08:12 +0100

    Get rid of localization, Use Chrome’s i18n framework later

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
-//SUPER DYNAMIC
+const searchPaths = {
+  "https://www.fragrantica.com": "search",
+  "https://www.fragrantica.de": "Suche",
+  "https://www.fragrantica.es": "buscar",
+  "https://www.fragrantica.pl": "szukaj",
+  "https://www.fragrantica.com.br": "busca",
+  "https://www.fragrantica.nl": "zoeken",
+  "https://www.punmiris.com": "parfemi",
+  "https://www.fragrantica.ro": "cautare",
+};
+
+// SUPER DYNAMIC
 chrome.runtime.onInstalled.addListener(() => {
   // Set default settings on installation
   chrome.storage.sync.set({
@@ -15,47 +26,16 @@ function updateContextMenu() {
   // Clear existing context menus
   chrome.contextMenus.removeAll(() => {
     chrome.storage.sync.get("useParfumoSearch", ({ useParfumoSearch }) => {
-      chrome.storage.sync.get("fragranticaLanguageUrl", ({ fragranticaLanguageUrl }) => {
-        const domain = fragranticaLanguageUrl || "https://www.fragrantica.com"; // Default to English
-        const title = getTitleByLanguage(domain, useParfumoSearch);
+      const title = useParfumoSearch ? "Search on Fragrantica + Parfumo" : "Search on Fragrantica";
 
-        // Create the context menu
-        chrome.contextMenus.create({
-          id: "searchFragrantica",
-          title,
-          contexts: ["selection"],
-        });
+      // Create the context menu
+      chrome.contextMenus.create({
+        id: "searchFragrantica",
+        title,
+        contexts: ["selection"],
       });
     });
   });
-}
-
-// Get the context menu title based on language selection
-function getTitleByLanguage(domain, useParfumoSearch) {
-  const languageTitles = {
-    "https://www.fragrantica.com": "Search on Fragrantica",
-    "https://www.fragrantica.de": "Auf Fragrantica suchen",
-    "https://www.fragrantica.es": "Buscar en Fragrantica",
-    "https://www.fragrantica.fr": "Chercher sur Fragrantica",
-    "https://www.fragrantica.cz": "Hledat na Fragrantica",
-    "https://www.fragrantica.it": "Cerca su Fragrantica",
-    "https://www.fragrantica.ru": "Поиск на Fragrantica",
-    "https://www.fragrantica.pl": "Szukaj na Fragrantica",
-    "https://www.fragrantica.com.br": "Buscar no Fragrantica",
-    "https://www.fragrantica.nl": "Zoeken op Fragrantica",
-    "https://www.punmiris.com": "Pretraga na Punmiris",
-    "https://www.fragrantica.ro": "Căutare pe Fragrantica",
-    "https://www.fragrantica.gr": "Αναζήτηση στο Fragrantica",
-    "https://www.fragrantica.asia": "搜索Fragrantica",
-    "https://www.fragrantica.ua": "Пошук на Fragrantica",
-    "https://www.fragrantica.mn": "Fragrantica дээр хайлт хийх",
-    "https://www.fragrantica.co.il": "חיפוש ב-Fragrantica",
-    "https://www.fragranticarabia.com": "البحث في فراغنتيكا",
-  };
-
-  const defaultTitle = languageTitles["https://www.fragrantica.com"];
-  const translatedTitle = languageTitles[domain] || defaultTitle;
-  return useParfumoSearch ? `${translatedTitle} + Parfumo` : translatedTitle;
 }
 
 // Listen for changes to the useParfumoSearch setting and update the context menu
@@ -64,18 +44,6 @@ chrome.storage.onChanged.addListener((changes) => {
     updateContextMenu();
   }
 });
-
-// Map domains to their specific search paths
-const searchPaths = {
-  "https://www.fragrantica.com": "search",
-  "https://www.fragrantica.de": "Suche",
-  "https://www.fragrantica.es": "buscar",
-  "https://www.fragrantica.pl": "szukaj",
-  "https://www.fragrantica.com.br": "busca",
-  "https://www.fragrantica.nl": "zoeken",
-  "https://www.punmiris.com": "parfemi",
-  "https://www.fragrantica.ro": "cautare",
-};
 
 // Handle context menu clicks
 chrome.contextMenus.onClicked.addListener((info) => {
